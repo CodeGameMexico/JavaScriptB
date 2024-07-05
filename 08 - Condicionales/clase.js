8e06b8871c8d638cfd8126a4ff5893dd4843dde2
noe1995 <noe.viveros.1995@gmail.com>
2024-07-05 12:42:27 -0600

    no message

--- 08 - Condicionales/clase.js
+++ 08 - Condicionales/clase.js
@@ -1,5 +1,12 @@
 /* Código de clase
-    El siguente código fue elaborado por CodeGame Academy con fines de educación,
-    cualquier mal uso del mismo, la academia no se hace responsable
-*/
+    En el siguente espacio, escribir el código o actividades realizadas en clase.
+    1.- El código deberá de esta escrito en tu totalidad 
+    2.- Si se necesita añador un archivo a parte, este no deberá de estar en formato ejecutable, si no en formato relacionado al curso.
+    3.- El código deberá estar relacionado con el tema de la clase / curso.
 
+    Para dudas, preguntas, asesoramientos comunicarse al correo: informacion@codegame.com.mx con el asunto "Ayuda con el Curso (nombre del curso)"
+
+    Recursos de la actividad:
+    1.- Introducción a JavaScript - Condicionales: https://makeitrealcamp.gitbook.io/javascript-book/condicionales
+    2.- mdn web docs - Tomando decisiones en tu código - condicionales: https://developer.mozilla.org/es/docs/Learn/JavaScript/Building_blocks/conditionals
+*/
--- 11 - Objetos/clase.js
+++ 11 - Objetos/clase.js
@@ -1,5 +1,13 @@
 /* Código de clase
-    El siguente código fue elaborado por CodeGame Academy con fines de educación,
-    cualquier mal uso del mismo, la academia no se hace responsable
-*/
+    En el siguente espacio, escribir el código o actividades realizadas en clase.
+    1.- El código deberá de esta escrito en tu totalidad 
+    2.- Si se necesita añador un archivo a parte, este no deberá de estar en formato ejecutable, si no en formato relacionado al curso.
+    3.- El código deberá estar relacionado con el tema de la clase / curso.
 
+    Para dudas, preguntas, asesoramientos comunicarse al correo: informacion@codegame.com.mx con el asunto "Ayuda con el Curso (nombre del curso)"
+
+    Recursos de la actividad:
+    1.- FreeCodeCamp - JavaScript Crear Objeto - Cómo definir Objetos en JS: https://www.freecodecamp.org/espanol/news/javascript-crear-objecto-como-definir-objetos-en-js/
+    2.- mdn web docs - Trabajando con objetos: https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Working_with_objects
+    3.- Manz.dev - ¿Que son los objetos?: https://lenguajejs.com/javascript/objetos/que-son/
+*/
