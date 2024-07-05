57b01280a05d02aca5a60ed8db4907f28fe26397
noe1995 <noe.viveros.1995@gmail.com>
2024-07-05 12:02:49 -0600

    Se añade docs a algunas clases

--- 03 - Hola mundo/clase.js
+++ 03 - Hola mundo/clase.js
@@ -1,5 +1,15 @@
 /* Código de clase
-    El siguente código fue elaborado por CodeGame Academy con fines de educación,
-    cualquier mal uso del mismo, la academia no se hace responsable
+    En el siguente espacio, escribir el código o actividades realizadas en clase.
+    1.- El código deberá de esta escrito en tu totalidad 
+    2.- Si se necesita añador un archivo a parte, este no deberá de estar en formato ejecutable, si no en formato relacionado al curso.
+    3.- El código deberá estar relacionado con el tema de la clase / curso.
+
+    Para dudas, preguntas, asesoramientos comunicarse al correo: informacion@codegame.com.mx con el asunto "Ayuda con el Curso (nombre del curso)"
+
+
+    Recursos de la actividad:
+    1.- DivCode - Hola Mundo JavaScript Visual Studio CODE: https://www.youtube.com/watch?v=tr5rsny9Gm8&ab_channel=divcode
+    2.- Oregoom - ¡Hola, mundo!, en JavaScript: https://oregoom.com/javascript/hola-mundo/#:~:text=Para%20crear%20un%20%E2%80%9CHola%20Mundo,en%20la%20consola%20de%20JavaScript.
+
 */
 
--- 04 - Variables y comentarios/clase.js
+++ 04 - Variables y comentarios/clase.js
@@ -1,5 +1,14 @@
 /* Código de clase
-    El siguente código fue elaborado por CodeGame Academy con fines de educación,
-    cualquier mal uso del mismo, la academia no se hace responsable
+    En el siguente espacio, escribir el código o actividades realizadas en clase.
+    1.- El código deberá de esta escrito en tu totalidad 
+    2.- Si se necesita añador un archivo a parte, este no deberá de estar en formato ejecutable, si no en formato relacionado al curso.
+    3.- El código deberá estar relacionado con el tema de la clase / curso.
+
+    Para dudas, preguntas, asesoramientos comunicarse al correo: informacion@codegame.com.mx con el asunto "Ayuda con el Curso (nombre del curso)"
+
+    Recursos de la actividad:
+    1.- KeepCoding - Declarar variables en JavaScript: https://keepcoding.io/blog/declarar-variables-en-javascript/#:~:text=Una%20variable%20en%20JavaScript%20es,el%20nombre%20cuando%20queramos%20usarlo.
+    2.- Manz.Dev - Comentarios de código: https://lenguajejs.com/fundamentos/introduccion/comentarios-de-codigo/
+    3.- Cursos de desarrollo - Curso de JavaScript: Comentarios y variables: https://cursosdedesarrollo.com/2013/12/curso-de-javascript-comentarios-y-variables/
 */
 
