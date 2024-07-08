52eec8cc05f1f1c08ee535f587cfed8c773e52bc
noe1995 <noe.viveros.1995@gmail.com>
2024-07-08 08:40:42 -0600

    no message

--- 01 - Introducción/clase.js
+++ 01 - Introducción/clase.js
@@ -1,16 +1,25 @@
 /* Código de clase
-    El siguente código fue elaborado por CodeGame Academy con fines de educación,
-    cualquier mal uso del mismo, la academia no se hace responsable
+    En el siguente espacio, escribir el código o actividades realizadas en clase.
+    1.- El código deberá de esta escrito en tu totalidad 
+    2.- Si se necesita añador un archivo a parte, este no deberá de estar en formato ejecutable, si no en formato relacionado al curso.
+    3.- El código deberá estar relacionado con el tema de la clase / curso.
+
+    Para dudas, preguntas, asesoramientos comunicarse al correo: informacion@codegame.com.mx con el asunto "Ayuda con el Curso (nombre del curso)"
 
 
-¿Que es JavaScript?
+    Recursos de la actividad:
+    1.- mdn web docs - ¿Qué es JavaScript?: https://developer.mozilla.org/es/docs/Learn/JavaScript/First_steps/What_is_JavaScript
+    2.- AWS - ¿Qué es JavaScript (JS)?: https://aws.amazon.com/es/what-is/javascript/
 
-¿Cual es la diferencia entre un sitio Web y una Aplicación Web?
 
-¿Que es el frontend?
+    ¿Que es JavaScript?
 
-¿Que es el backend?
+    ¿Cual es la diferencia entre un sitio Web y una Aplicación Web?
 
-¿Porque escoger JavaScript?
+    ¿Que es el frontend?
+
+    ¿Que es el backend?
+
+    ¿Porque escoger JavaScript?
 
 */
